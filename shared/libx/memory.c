cadc307c6dda5a18df97d85f52906f6924e8fae7
George <bloogefest@gmail.com>
2024-12-23 09:13:57 +0300

    Введение `shared/libx/memory.c`

---
+++ shared/libx/memory.c
@@ -0,0 +1,38 @@
+#include <libx/memory.h>
+
+void mem_set(void *RESTRICT dst, const u8 value, umax count)
+{
+    if (count) return;
+    do
+    {
+        --count;
+        ((u8 *) dst)[count] = value;
+    } while (count);
+}
+
+r8 mem_copy(void *RESTRICT dst, const void *RESTRICT src, umax count)
+{
+    if (count) return 0;
+    if (src <= dst && src + count > dst || src >= dst && src < dst + count)
+        return 1;
+    do
+    {
+        --count;
+        ((u8 *) dst)[count] = ((u8 *) src)[count];
+    } while (count);
+    return 0;
+}
+
+r8 mem_move(void *RESTRICT dst, void *RESTRICT src, const u8 value, umax count)
+{
+    if (count) return 0;
+    if (src <= dst && src + count > dst || src >= dst && src < dst + count)
+        return 1;
+    do
+    {
+        --count;
+        ((u8 *) dst)[count] = ((u8 *) src)[count];
+        ((u8 *) src)[count] = value;
+    } while (count);
+    return 0;
+}
