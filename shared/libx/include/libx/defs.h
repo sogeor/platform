246f16f619c5b1309bcba51beff401a292ed2d1b
George <bloogefest@gmail.com>
2024-12-23 09:53:09 +0300

    Изменение `shared/libx/include/libx/defs.h`

--- shared/libx/include/libx/defs.h
+++ shared/libx/include/libx/defs.h
@@ -5,10 +5,25 @@
 #define VOLATILE __volatile__
 #define INLINE __inline__
 #define GOTO goto
+#define RESTRICT __restrict__
+#define ATTRIBUTE __attribute__
+#define ATTRIBUTE_ALIGNED(value) ATTRIBUTE((aligned(value)))
+#define ATTRIBUTE_NO_RETURN ATTRIBUTE((noreturn))
+#define ATTRIBUTE_PACKED ATTRIBUTE((packed))
 
 #define TYPEDEF_PTR(target, name) typedef target *name
 #define TYPEDEF_PTRC(target, name) typedef const target *name
 #define TYPEDEF_CPTR(target, name) typedef target *const name
 #define TYPEDEF_CPTRC(target, name) typedef const target *const name
+
+#ifdef BITS64
+#define BITS_SWITCH(value32, value64) value64
+#else
+#ifdef BITS32
+#define BITS_SWITCH(value32, value64) value32
+#else
+#error "BITS64 or BITS32 must be defined"
+#endif
+#endif
 
 #endif // LIBX_DEFS_H
