a0ee6f1cfcf1e21f3bb2a1169b5a1ada9db41a6c
George <bloogefest@gmail.com>
2024-12-23 16:35:21 +0300

    Изменение `shared/libx/memory.c`

--- shared/libx/memory.c
+++ shared/libx/memory.c
@@ -1,36 +1,14 @@
 #include <libx/memory.h>
 
-void mem_set(const ptr dst, const u8 value, umax count)
+void mem_set(void *dst, const u8 value, umax count)
 {
-    if (!count) return;
-    do
-    {
-        --count;
-        ((u8 *) dst)[count] = value;
-    } while (count);
+    while (count) ((u8 *) dst)[--count] = value;
 }
 
-r8 mem_copy(const rptr dst, const rptrc src, umax count)
+r8 mem_copy(void *__restrict__ dst, const void *__restrict__ src, umax count)
 {
     if (!count) return 0;
     if (src <= dst && src + count > dst || src >= dst && src < dst + count) return 1;
-    do
-    {
-        --count;
-        ((u8 *) dst)[count] = ((u8 *) src)[count];
-    } while (count);
-    return 0;
-}
-
-r8 mem_move(const rptr dst, const rptr src, const u8 value, umax count)
-{
-    if (!count) return 0;
-    if (src <= dst && src + count > dst || src >= dst && src < dst + count) return 1;
-    do
-    {
-        --count;
-        ((u8 *) dst)[count] = ((u8 *) src)[count];
-        ((u8 *) src)[count] = value;
-    } while (count);
+    while (count) ((u8 *) dst)[--count] = ((u8 *) src)[count];
     return 0;
 }
