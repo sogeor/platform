50c7d77db098bdbc60e3364bb258ecd402c7630d
George <bloogefest@gmail.com>
2024-12-23 16:48:06 +0300

    Изменение `shared/kernel/stream.c`

--- shared/kernel/stream.c
+++ shared/kernel/stream.c
@@ -4,18 +4,18 @@
 umax stream_count_created = 0;
 umax stream_count_opened = 0;
 
-stream_t *RESTRICT stream_open()
+stream_t *stream_open()
 {
-    stream_t *RESTRICT stream = memory_allocate_unaligned(sizeof(stream_t));
-    if (stream == NULL_RPTR) return NULL_RPTR;
+    stream_t *stream = memory_allocate_unaligned(sizeof(stream_t));
+    if (stream == NULL) return NULL;
     *(umax *) &stream->desc = stream_count_created++;
     ++stream_count_opened;
     return stream;
 }
 
-r8 stream_close(stream_t *RESTRICT stream)
+r8 stream_close(stream_t *stream)
 {
-    if (stream == NULL_RPTR || stream->desc >= stream_count_created) return 1;
+    if (stream == NULL || stream->desc >= stream_count_created) return 1;
     if (memory_free(stream)) return 2;
     --stream_count_opened;
     return 0;
