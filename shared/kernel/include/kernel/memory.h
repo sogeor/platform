0a0902af6c602b8228ab20bd902a4cad2d5479e9
George <bloogefest@gmail.com>
2024-12-23 16:42:59 +0300

    Изменение `shared/kernel/include/kernel/memory.h`

--- shared/kernel/include/kernel/memory.h
+++ shared/kernel/include/kernel/memory.h
@@ -5,34 +5,24 @@
 
 /*!
  * \brief Выделяет целевую область памяти — количество \code count\endcode байт.
- * \code
- * ptr dst = memory_allocate_unaligned(1024);
- * \endcode
  * \param[in] count Количество байт, которое необходимо выделить.
- * \return \code NULL_RPTR\endcode — недостаточно свободной области памяти; \code ?\endcode — целевая область памяти.
+ * \return \code NULL\endcode — недостаточно свободной области памяти; \code ?\endcode — целевая область памяти.
  */
-rptr memory_allocate_unaligned(umax count);
+void *memory_allocate_unaligned(umax count);
 
 /*!
  * \brief Выделяет целевую область памяти — выровненное по \code aligment\endcode количество \code count\endcode байт.
- * \code
- * ptr dst = memory_allocate_aligned(1024, 4096);
- * \endcode
  * \param[in] count Количество байт, которое необходимо выделить.
  * \param[in] alignment Количество байт, по которым необходимо выровнять.
- * \return \code NULL_RPTR\endcode — недостаточно свободной области памяти; \code ?\endcode — целевая область памяти.
+ * \return \code NULL\endcode — недостаточно свободной области памяти; \code ?\endcode — целевая область памяти.
  */
-rptr memory_allocate_aligned(umax count, umax alignment);
+void *memory_allocate_aligned(umax count, umax alignment);
 
 /*!
- * \brief Освобождает \code dst\endcode.
- * \code
- * ptr dst;
- * memory_free(dst);
- * \endcode
- * \param[in] dst Целевая область памяти.
- * \return \code 0\endcode — успешно; \code 1\endcode — некорректная целевая область памяти.
+ * \brief Освобождает \code ptr\endcode.
+ * \param[in] ptr Исходную область памяти.
+ * \return \code 0\endcode — успешно; \code 1\endcode — некорректная \code ptr\endcode.
  */
-r8 memory_free(ptr dst);
+r8 memory_free(void *ptr);
 
 #endif // KERNEL_MEMORY_H
