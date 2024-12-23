c23a376f8e1fc654479253741cb46aab71a16c35
George <bloogefest@gmail.com>
2024-12-23 14:24:46 +0300

    Изменение `shared/libx/include/libx/strings.h`

--- shared/libx/include/libx/strings.h
+++ shared/libx/include/libx/strings.h
@@ -19,7 +19,7 @@ typedef struct str8
      */
     c8 data[];
 }
-    ATTRIBUTE_PACKED str8;
+    ATTRIBUTE_PACKED str8_t;
 
 /*!
  * \brief Заменяет, начиная с \code offset\endcode, \code count\endcode символов в \code dst\endcode на \code value\endcode.
@@ -33,7 +33,7 @@ typedef struct str8
  * \param[in] count Количество символов, которое необходимо заменить.
  * \return \code 0\endcode — успешно; \code 1\endcode — сумма \code offset\endcode и \code count\endcode больше длины \code dst\endcode.
  */
-r8 str8_set(str8 dst, c8 value, umax offset, umax count);
+r8 str8_set(str8_t dst, c8 value, umax offset, umax count);
 
 /*!
  * \brief Копирует, начиная с \code src_offset\endcode и \code dst_offset\endcode, \code count\endcode символов из \code src\endcode в \code dst\endcode.
@@ -49,7 +49,7 @@ r8 str8_set(str8 dst, c8 value, umax offset, umax count);
  * \param[in] count Количество символов, которое необходимо скопировать.
  * \return \code 0\endcode — успешно; \code 1\endcode — области данных \code dst\endcode и \code src\endcode пересекаются; \code 2\endcode — сумма \code src_offset\endcode и \code count\endcode больше длины \code src\endcode; \code 3\endcode — сумма \code dst_offset\endcode и \code count\endcode больше длины \code dst\endcode.
  */
-r8 str8_copy(str8 dst, str8 src, umax dst_offset, umax src_offset, umax count);
+r8 str8_copy(str8_t dst, str8_t src, umax dst_offset, umax src_offset, umax count);
 
 /*!
  * \brief Копирует, начиная с \code src_offset\endcode и \code dst_offset\endcode, \code count\endcode символов из \code src\endcode в \code dst\endcode и заменяет, начиная с \code src_offset\endcode, \code count\endcode символов в \code src\endcode на \code value\endcode.
@@ -66,7 +66,7 @@ r8 str8_copy(str8 dst, str8 src, umax dst_offset, umax src_offset, umax count);
  * \param[in] count Количество символов, которое необходимо скопировать.
  * \return \code 0\endcode — успешно; \code 1\endcode — области данных \code dst\endcode и \code src\endcode пересекаются; \code 2\endcode — сумма \code src_offset\endcode и \code count\endcode больше длины \code src\endcode; \code 3\endcode — сумма \code dst_offset\endcode и \code count\endcode больше длины \code dst\endcode.
  */
-r8 str8_move(str8 dst, str8 src, c8 value, umax dst_offset, umax src_offset, umax count);
+r8 str8_move(str8_t dst, str8_t src, c8 value, umax dst_offset, umax src_offset, umax count);
 
 /*!
  * \brief Строка, которая состоит из 2-байтовых символов.
@@ -83,7 +83,7 @@ typedef struct str16
      */
     c16 data[];
 }
-    ATTRIBUTE_PACKED str16;
+    ATTRIBUTE_PACKED str16_t;
 
 /*!
  * \brief Заменяет, начиная с \code offset\endcode, \code count\endcode символов в \code dst\endcode на \code value\endcode.
@@ -97,7 +97,7 @@ typedef struct str16
  * \param[in] count Количество символов, которое необходимо заменить.
  * \return \code 0\endcode — успешно; \code 1\endcode — сумма \code offset\endcode и \code count\endcode больше длины \code dst\endcode.
  */
-r8 str16_set(str16 dst, c16 value, umax offset, umax count);
+r8 str16_set(str16_t dst, c16 value, umax offset, umax count);
 
 /*!
  * \brief Копирует, начиная с \code src_offset\endcode и \code dst_offset\endcode, \code count\endcode символов из \code src\endcode в \code dst\endcode.
@@ -113,7 +113,7 @@ r8 str16_set(str16 dst, c16 value, umax offset, umax count);
  * \param[in] count Количество символов, которое необходимо скопировать.
  * \return \code 0\endcode — успешно; \code 1\endcode — области данных \code dst\endcode и \code src\endcode пересекаются; \code 2\endcode — сумма \code src_offset\endcode и \code count\endcode больше длины \code src\endcode; \code 3\endcode — сумма \code dst_offset\endcode и \code count\endcode больше длины \code dst\endcode.
  */
-r8 str16_copy(str16 dst, str16 src, umax dst_offset, umax src_offset, umax count);
+r8 str16_copy(str16_t dst, str16_t src, umax dst_offset, umax src_offset, umax count);
 
 /*!
  * \brief Копирует, начиная с \code src_offset\endcode и \code dst_offset\endcode, \code count\endcode символов из \code src\endcode в \code dst\endcode и заменяет, начиная с \code src_offset\endcode, \code count\endcode символов в \code src\endcode на \code value\endcode.
@@ -130,7 +130,7 @@ r8 str16_copy(str16 dst, str16 src, umax dst_offset, umax src_offset, umax count
  * \param[in] count Количество символов, которое необходимо скопировать.
  * \return \code 0\endcode — успешно; \code 1\endcode — области данных \code dst\endcode и \code src\endcode пересекаются; \code 2\endcode — сумма \code src_offset\endcode и \code count\endcode больше длины \code src\endcode; \code 3\endcode — сумма \code dst_offset\endcode и \code count\endcode больше длины \code dst\endcode.
  */
-r8 str16_move(str16 dst, str16 src, c16 value, umax dst_offset, umax src_offset, umax count);
+r8 str16_move(str16_t dst, str16_t src, c16 value, umax dst_offset, umax src_offset, umax count);
 
 /*!
  * \brief Строка, которая состоит из 4-байтовых символов.
@@ -147,7 +147,7 @@ typedef struct str32
      */
     c32 data[];
 }
-    ATTRIBUTE_PACKED str32;
+    ATTRIBUTE_PACKED str32_t;
 
 /*!
  * \brief Заменяет, начиная с \code offset\endcode, \code count\endcode символов в \code dst\endcode на \code value\endcode.
@@ -161,7 +161,7 @@ typedef struct str32
  * \param[in] count Количество символов, которое необходимо заменить.
  * \return \code 0\endcode — успешно; \code 1\endcode — сумма \code offset\endcode и \code count\endcode больше длины \code dst\endcode.
  */
-r8 str32_set(str16 dst, c32 value, umax offset, umax count);
+r8 str32_set(str16_t dst, c32 value, umax offset, umax count);
 
 /*!
  * \brief Копирует, начиная с \code src_offset\endcode и \code dst_offset\endcode, \code count\endcode символов из \code src\endcode в \code dst\endcode.
@@ -177,7 +177,7 @@ r8 str32_set(str16 dst, c32 value, umax offset, umax count);
  * \param[in] count Количество символов, которое необходимо скопировать.
  * \return \code 0\endcode — успешно; \code 1\endcode — области данных \code dst\endcode и \code src\endcode пересекаются; \code 2\endcode — сумма \code src_offset\endcode и \code count\endcode больше длины \code src\endcode; \code 3\endcode — сумма \code dst_offset\endcode и \code count\endcode больше длины \code dst\endcode.
  */
-r8 str32_copy(str32 dst, str32 src, umax dst_offset, umax src_offset, umax count);
+r8 str32_copy(str32_t dst, str32_t src, umax dst_offset, umax src_offset, umax count);
 
 /*!
  * \brief Копирует, начиная с \code src_offset\endcode и \code dst_offset\endcode, \code count\endcode символов из \code src\endcode в \code dst\endcode и заменяет, начиная с \code src_offset\endcode, \code count\endcode символов в \code src\endcode на \code value\endcode.
@@ -194,6 +194,6 @@ r8 str32_copy(str32 dst, str32 src, umax dst_offset, umax src_offset, umax count
  * \param[in] count Количество символов, которое необходимо скопировать.
  * \return \code 0\endcode — успешно; \code 1\endcode — области данных \code dst\endcode и \code src\endcode пересекаются; \code 2\endcode — сумма \code src_offset\endcode и \code count\endcode больше длины \code src\endcode; \code 3\endcode — сумма \code dst_offset\endcode и \code count\endcode больше длины \code dst\endcode.
  */
-r8 str32_move(str32 dst, str32 src, c32 value, umax dst_offset, umax src_offset, umax count);
+r8 str32_move(str32_t dst, str32_t src, c32 value, umax dst_offset, umax src_offset, umax count);
 
 #endif // LIBX_STRINGS_H
