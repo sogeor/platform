d7e5bffd02ffddf795de9ed5b30e58dd737a7c12
George <bloogefest@gmail.com>
2024-12-23 16:35:04 +0300

    Изменение `shared/libx/include/libx/memory.h`

--- shared/libx/include/libx/memory.h
+++ shared/libx/include/libx/memory.h
@@ -1,48 +1,23 @@
 #ifndef LIBX_MEMORY_H
 #define LIBX_MEMORY_H
 
-#include <libx/defs.h>
 #include <libx/types.h>
 
 /*!
  * \brief Заменяет \code count\endcode байт в \code dst\endcode на \code value\endcode.
- * \code
- * u8 dst[1024];
- * mem_set(dst, 0, 1024);
- * \endcode
  * \param[out] dst Целевая область памяти.
  * \param[in] value Значение, на которое заменяется каждый байт.
  * \param[in] count Количество байт, которое необходимо заменить.
  */
-void mem_set(ptr dst, u8 value, umax count);
+void mem_set(void *dst, u8 value, umax count);
 
 /*!
  * \brief Копирует \code count\endcode байт из \code src\endcode в \code dst\endcode.
- * \code
- * u8 dst[1024];
- * u8 src[1024];
- * mem_copy(dst, src, 1024);
- * \endcode
  * \param[out] dst Целевая область памяти.
  * \param[in] src Исходная область памяти.
  * \param[in] count Количество байт, которое необходимо скопировать.
  * \return \code 0\endcode — успешно; \code 1\endcode — \code dst\endcode и \code src\endcode пересекаются.
  */
-r8 mem_copy(rptr dst, rptrc src, umax count);
-
-/*!
- * \brief Копирует \code count\endcode байт из \code src\endcode в \code dst\endcode и заменяет \code count\endcode байт в \code src\endcode на \code value\endcode.
- * \code
- * u8 dst[1024];
- * u8 src[1024];
- * mem_move(dst, src, 0, 1024);
- * \endcode
- * \param[out] dst Целевая область памяти.
- * \param[in,out] src Исходная область памяти.
- * \param[in] value Значение, на которое заменяется каждый байт в \code src\endcode.
- * \param[in] count Количество байт, которое необходимо скопировать.
- * \return \code 0\endcode — успешно; \code 1\endcode — \code dst\endcode и \code src\endcode пересекаются.
- */
-r8 mem_move(rptr dst, rptr src, u8 value, umax count);
+r8 mem_copy(void *__restrict__ dst, const void *__restrict__ src, umax count);
 
 #endif // LIBX_MEMORY_H
