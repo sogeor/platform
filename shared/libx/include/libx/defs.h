169cacf798a70995bc6d2c83685010de0f6659e2
George <bloogefest@gmail.com>
2024-12-23 10:28:04 +0300

    Изменение `shared/libx/include/libx/defs.h`

--- shared/libx/include/libx/defs.h
+++ shared/libx/include/libx/defs.h
@@ -11,10 +11,10 @@
 #define ATTRIBUTE_NO_RETURN ATTRIBUTE((noreturn))
 #define ATTRIBUTE_PACKED ATTRIBUTE((packed))
 
-#define TYPEDEF_PTR(target, name) typedef target *name
-#define TYPEDEF_PTRC(target, name) typedef const target *name
-#define TYPEDEF_CPTR(target, name) typedef target *const name
-#define TYPEDEF_CPTRC(target, name) typedef const target *const name
+#define TYPEDEF_PTR(target, name) typedef target *RESTRICT name
+#define TYPEDEF_PTRC(target, name) typedef const target *RESTRICT name
+#define TYPEDEF_CPTR(target, name) typedef target *const RESTRICT name
+#define TYPEDEF_CPTRC(target, name) typedef const target *const RESTRICT name
 
 #ifdef BITS64
 #define BITS_SWITCH(value32, value64) value64
