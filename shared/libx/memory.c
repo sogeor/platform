ad2b0724f78515f16bbd6ee08e7396126d44b19e
George <bloogefest@gmail.com>
2024-12-24 15:49:42 +0300

    Изменение `shared/libx/memory.c`

--- shared/libx/memory.c
+++ shared/libx/memory.c
@@ -1,14 +1,37 @@
 #include <libx/memory.h>
 
-void mem_set(void *dst, const u8 value, umax count)
+r8 memory_set(void *dst, const u8 value, umax count)
 {
+    if (dst == NULL) return 1;
     while (count) ((u8 *) dst)[--count] = value;
+    return 0;
 }
 
-r8 mem_copy(void *__restrict__ dst, const void *__restrict__ src, umax count)
+r8 memory_copy(void *dst, const void *src, const umax count)
 {
-    if (!count) return 0;
-    if (src <= dst && src + count > dst || src >= dst && src < dst + count) return 1;
+    const r8 rslt = memory_copy_unbuffered(dst, src, count);
+    return rslt == 3 ? memory_copy_buffered(dst, src, count) : rslt;
+}
+
+r8 memory_copy_unbuffered(void *__restrict__ dst, const void *__restrict__ src, umax count)
+{
+    if (dst == NULL) return 1;
+    if (src == NULL) return 2;
+    if (src <= dst && src + count > dst || src >= dst && src < dst + count) return 3;
     while (count) ((u8 *) dst)[--count] = ((u8 *) src)[count];
     return 0;
+}
+
+r8 memory_copy_buffered(void *dst, const void *src, const umax count)
+{
+    if (dst == NULL) return 1;
+    if (src == NULL) return 2;
+    if (!count) return 0;
+    u8 *tmp;
+    if (MEMORY_ALLOCATE_UNALIGNED(&tmp, count)) return 3;
+    umax i = count;
+    while (i) tmp[--i] = ((u8 *) src)[i];
+    i = count;
+    while (i) ((u8 *) dst)[--i] = tmp[i];
+    return MEMORY_FREE(&tmp) ? 4 : 0;
 }
