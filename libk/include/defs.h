a935ca6774bb8a3cb841e268fe74ff059df8aeb6
George <bloogefest@gmail.com>
2025-02-04 02:41:20 +0300

    Подготовка к разработке `libk`

---
+++ libk/include/defs.h
@@ -0,0 +1,6 @@
+#ifndef LIBK_DEFS_H
+#define LIBK_DEFS_H
+
+// TODO: libk/defs.h
+
+#endif // LIBK_DEFS_H
---
+++ libk/include/x86/defs.h
@@ -0,0 +1,6 @@
+#ifndef LIBK_X86_DEFS_H
+#define LIBK_X86_DEFS_H
+
+// TODO: libk/x86/defs.h
+
+#endif // LIBK_X86_DEFS_H
